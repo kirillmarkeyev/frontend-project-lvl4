58cfff79bdb508cc0352eb0bda85411fd095efcc
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-07-31 21:13:49 +0300

    Step 3: update NotFoundPage.jsx

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
+import notFoundPic from '../assets/404.svg';
+
 const NotFoundPage = () => (
-  <div>
-    <div>
-      <h1>Страница не найдена</h1>
-    </div>
-    <div>
-      <p>
-        Но вы можете перейти
-        <a href="/"> на главную страницу</a>
-      </p>
-    </div>
+  <div className="text-center">
+    <img alt="Страница не найдена" className="img-fluid h-25" src={notFoundPic} style={{ width: 300 }} />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      <a href="/"> на главную страницу</a>
+    </p>
   </div>
 );
 
