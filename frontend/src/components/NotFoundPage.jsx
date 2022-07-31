3d733a9086e91ee43ae98a48ed2d71cd310e0a21
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-07-31 21:21:13 +0300

    Step 3: fix NotFoundPage.jsx

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import notFoundPic from '../assets/404.svg';
 
 const NotFoundPage = () => (
   <div className="text-center">
-    <img alt="Страница не найдена" className="img-fluid h-25" src={notFoundPic} style={{ width: 300 }} />
+    <img alt="Страница не найдена" className="img-fluid h-25" src={notFoundPic} />
     <h1 className="h4 text-muted">Страница не найдена</h1>
     <p className="text-muted">
       Но вы можете перейти
