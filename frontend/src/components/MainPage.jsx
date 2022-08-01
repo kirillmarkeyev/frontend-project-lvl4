92a96d276fb54147e1058d5a69a65c2bcd100201
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-02 00:27:34 +0300

    Step 4: update MainPage.jsx

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+import Channels from './Channels.jsx';
+import Messages from './Messages.jsx';
 
 import useAuth from '../hooks/index.js';
 import routes from '../routes.js';
@@ -17,9 +21,12 @@ const MainPage = () => {
   }, []);
 
   return (
-    <div>
-        Under construction
-    </div>
+    <Container className="h-100 my-4 overflow-hidden rounded shadow">
+      <Row className="h-100 bg-white flex-md-row">
+        <Channels />
+        <Messages />
+      </Row>
+    </Container>
   );
 };
 
