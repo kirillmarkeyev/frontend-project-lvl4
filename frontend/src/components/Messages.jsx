5be6c35c4194ef93f06b39a3402e367c4c88152b
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-03 22:09:35 +0300

    Step 4: update Messages.jsx

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { ArrowRightSquare } from 'react-bootstrap-icons';
 
+import { useSelector } from 'react-redux';
+import { selectors as channelsSelectors } from '../slices/channelsSlice.js';
+import { selectors as messagesSelectors } from '../slices/messagesSlice.js';
+
 const Messages = () => {
+  const [message, setMessage] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(message);
+    setMessage('');
+  };
+  
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const channels = useSelector(channelsSelectors.selectAll);
+  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+  const currentChannel = channels.find((channel) => channel.id === currentChannelId);
+  console.log(currentChannel);
+
+  const messages = useSelector(messagesSelectors.selectAll);
+  console.log(messages);
+
   return (
     <div className="col p-0 h-100">
       <div className="d-flex flex-column h-100">
         <div className="bg-light mb-4 p-3 shadow-sm small">
-          <p className="m-0"><b># general</b></p>
-          <span className="text-muted">2 сообщения</span>
+          <p className="m-0"><b># {currentChannel.name}</b></p>
+          <span className="text-muted">{messages.length} сообщения</span>
         </div>
         <div id="messages-box" className="chat-messages overflow-auto px-5 ">
           <div className="text-break mb-2">
@@ -18,14 +47,16 @@ const Messages = () => {
           </div>
         </div>
         <div className="mt-auto px-5 py-3">
-          <form noValidate="" className="py-1 border rounded-2">
+          <form onSubmit={handleSubmit} noValidate="" className="py-1 border rounded-2">
             <div className="input-group has-validation">
               <input
+                onChange={handleChange}
                 name="body"
                 aria-label="Новое сообщение"
                 placeholder="Введите сообщение..."
                 className="border-0 p-0 ps-2 form-control"
-                value=""
+                value={message}
+                ref={inputRef}
               />
               <button type="submit" className="btn btn-group-vertical" disabled="">
                 <ArrowRightSquare />
