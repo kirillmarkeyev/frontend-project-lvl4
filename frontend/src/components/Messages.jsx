59a9fe18393b899d8ddab3ac292333a2b3b229d8
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-05 00:50:22 +0300

    Step 5: realize chat

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,13 +1,27 @@
 import React, { useState, useRef, useEffect } from 'react';
+import io from 'socket.io-client';
+
 import { ArrowRightSquare } from 'react-bootstrap-icons';
 
-import { useSelector } from 'react-redux';
+import useAuth from '../hooks/index.js';
+import { useSelector, useDispatch } from 'react-redux';
 import { selectors as channelsSelectors } from '../slices/channelsSlice.js';
 import { selectors as messagesSelectors } from '../slices/messagesSlice.js';
+import { actions as messagesActions } from '../slices/messagesSlice.js';
 
 const Messages = () => {
   const [message, setMessage] = useState('');
   const inputRef = useRef();
+  const auth = useAuth();
+  const dispatch = useDispatch();
+
+  const socket = io();
+
+  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+  const currentChannel = useSelector((state) => channelsSelectors.selectById(state, currentChannelId));
+
+  const messages = useSelector(messagesSelectors.selectAll);
+  const currentMessages = messages.filter((message) => message.channelId === currentChannelId);
 
   useEffect(() => {
     inputRef.current.focus();
@@ -15,33 +29,54 @@ const Messages = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(message);
-    setMessage('');
+    if (message) {
+      const body = message;
+      const channelId = currentChannelId;
+      const { username } = auth.user;
+      const data = {
+        body,
+        channelId,
+        username,
+      };
+      socket.emit('newMessage', data, (response) => {
+        console.log(response.status);
+      });
+      // https://socket.io/docs/v4/emitting-events/#acknowledgements
+
+      setMessage('');
+
+      socket.on('newMessage', (payload) => {
+        dispatch(messagesActions.addMessage(payload));
+      }); 
+    }
   };
 
   const handleChange = (e) => {
     setMessage(e.target.value);
   };
 
-  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
-  const currentChannel = useSelector((state) => channelsSelectors.selectById(state, currentChannelId));
-
-  const messages = useSelector(messagesSelectors.selectAll);
-
+  const messagesRender = () => {
+    if (currentMessages.length === 0) {
+      return null;
+    }
+    return (
+      currentMessages.map((message) => (
+        <div key={message.id} className="text-break mb-2">
+            <b>{message.username}</b>: {message.body}
+          </div>
+      ))
+    );
+  };
+  
   return (
     <div className="col p-0 h-100">
       <div className="d-flex flex-column h-100">
         <div className="bg-light mb-4 p-3 shadow-sm small">
           <p className="m-0"><b># {currentChannel?.name}</b></p>
-          <span className="text-muted">{messages.length} сообщений</span>
+          <span className="text-muted">{currentMessages.length} сообщений</span>
         </div>
         <div id="messages-box" className="chat-messages overflow-auto px-5 ">
-          <div className="text-break mb-2">
-            <b>admin</b>: Test message
-          </div>
-          <div className="text-break mb-2">
-            <b>admin</b>: One more test message
-          </div>
+          {messagesRender()}
         </div>
         <div className="mt-auto px-5 py-3">
           <form onSubmit={handleSubmit} noValidate="" className="py-1 border rounded-2">
--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -5,7 +5,7 @@ import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
 const channelsAdapter = createEntityAdapter();
 
 const initialState = channelsAdapter.getInitialState({
-  currentChannelId: null,   
+  currentChannelId: 1,   
 });
 
 const channelsSlice = createSlice({
