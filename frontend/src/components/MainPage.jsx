24e445c8987192f253fa838de0749018432c5587
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-05 23:29:57 +0300

    Step 6: update MainPage.jsx

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Container, Row } from 'react-bootstrap';
 
@@ -8,13 +8,18 @@ import Messages from './Messages.jsx';
 
 import useAuth from '../hooks/index.js';
 import routes from '../routes.js';
+import getModal from './modals/index.js';
 
 import { actions as channelsActions } from '../slices/channelsSlice.js';
 import { actions as messagesActions } from '../slices/messagesSlice.js';
 
 const MainPage = () => {
+  const [modalType, setModalType] = useState(null);
   const auth = useAuth();
   const dispatch = useDispatch();
+
+  const showModal = (type) => setModalType(type);
+  const hideModal = () => setModalType(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,12 +34,21 @@ const MainPage = () => {
     fetchData();
   }, []);
 
+  const renderModal = (type, hide) => {
+    if (!type) {
+      return null;
+    }
+    const Modal = getModal(type);
+    return <Modal onHide={hide} />;
+  };
+
   return (
     <Container className="h-100 my-4 overflow-hidden rounded shadow">
       <Row className="h-100 bg-white flex-md-row">
-        <Channels />
+        <Channels showModal={showModal} />
         <Messages />
       </Row>
+      {renderModal(modalType, hideModal)}
     </Container>
   );
 };
