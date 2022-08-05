14c6160adb9506e213a760cffe2d62312213a51e
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-05 23:29:17 +0300

    Step 6: update Channels.jsx

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -3,10 +3,10 @@ import cn from 'classnames';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { PlusSquare } from 'react-bootstrap-icons';
-import { selectors as channelsSelectors } from '../slices/channelsSlice.js';
-import { actions as channelsActions } from '../slices/channelsSlice.js';
+import { actions as channelsActions, selectors as channelsSelectors } from '../slices/channelsSlice.js';
 
-const Channels = () => {
+const Channels = (props) => {
+  const { showModal } = props;
   const channels = useSelector(channelsSelectors.selectAll);
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
 
@@ -37,11 +37,16 @@ const Channels = () => {
     );
   };
 
+  const handleAddChannel = () => {
+    console.log('Adding new channel');
+    showModal('adding');
+  };
+
   return (
     <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
       <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
         <span>Каналы</span>
-        <button type="button" className="p-0 text-primary btn btn-group-vertical">
+        <button onClick={handleAddChannel} type="button" className="p-0 text-primary btn btn-group-vertical">
           <PlusSquare />
           <span className="visually-hidden">+</span>
         </button>
