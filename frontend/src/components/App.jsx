6508cc4e344d3878ac970d39286e4641e87e8517
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-06 22:18:43 +0300

    Step 6: fix App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -61,8 +61,8 @@ const MainPageRoute = ({ children }) => {
 const App = () => (
   <StoreProvider store={store}>
     <AuthProvider>
-      <BrowserRouter>
-        <div className="d-flex flex-column h-100">
+      <div className="d-flex flex-column h-100">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={(
               <MainPageRoute>
@@ -72,8 +72,8 @@ const App = () => (
             <Route path="/login" element={<LoginPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </div>
     </AuthProvider>
   </StoreProvider>
 );
