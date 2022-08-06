65a6205e451bb5cc0d5e5abd2c0c25ff060589cf
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-06 18:18:37 +0300

    Step 6: update Add.jsx

--- frontend/src/components/modals/Add.jsx
+++ frontend/src/components/modals/Add.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
+import * as yup from 'yup';
 import { useFormik } from 'formik';
 import { Modal, Form, Button } from 'react-bootstrap';
+
+import { useSelector } from 'react-redux';
+import { selectors as channelsSelectors } from '../../slices/channelsSlice.js';
 
 const Add = (props) => {
   const { onHide } = props;
@@ -10,12 +14,26 @@ const Add = (props) => {
     inputEl.current.focus();
   }, []);
 
+  const channels = useSelector(channelsSelectors.selectAll);
+  
+  const validationSchema = yup.object().shape({
+    name: yup
+      .string()
+      .trim()
+      .min(3, 'От 3 до 20 символов')
+      .max(20, 'От 3 до 20 символов')
+      .notOneOf(channels.map((channel) => channel.name), 'Должно быть уникальным')
+      .required('Обязательное поле'),
+  });
+
   const formik = useFormik({
     initialValues: {
       name: '',
     },
+    validationSchema,
     onSubmit: (values) => {
       console.log(values);
+      onHide();
     },
   });
 
@@ -34,9 +52,12 @@ const Add = (props) => {
             id="name"
             name="name"
             value={formik.values.name}
+            isInvalid={formik.errors.name && formik.touched.name}
           />
             <Form.Label htmlFor="name" className="visually-hidden">Имя канала</Form.Label>
-
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.name}
+            </Form.Control.Feedback>
           <div className="d-flex justify-content-end">
             <Button className="me-2" variant="secondary" onClick={onHide}>Отменить</Button>
             <Button type="submit" variant="primary">Отправить</Button>
