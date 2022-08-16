b8c05196e2cddfba629ec7cff0857e17197b7ee3
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-16 21:45:18 +0300

    Code-review 1: update components

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable react/jsx-no-constructed-context-values */
-
-import React, { useState } from 'react';
+import React from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -15,44 +13,8 @@ import RegistrationPage from './RegistrationPage.jsx';
 import MainPage from './MainPage.jsx';
 import NotFoundPage from './NotFoundPage.jsx';
 
-import { AuthContext } from '../contexts/index.js';
+import AuthProvider from '../contexts/AuthProvider.jsx';
 import { useAuth } from '../hooks/index.js';
-
-// https://ru.hexlet.io/challenges/js_react_auth_exercise
-
-const AuthProvider = ({ children }) => {
-  const currentUser = JSON.parse(localStorage.getItem('user'));
-  const [user, setUser] = useState(currentUser ? { username: currentUser.username } : null);
-
-  const logIn = (userData) => {
-    localStorage.setItem('user', JSON.stringify(userData));
-    setUser({ username: userData.username });
-  };
-
-  const logOut = () => {
-    localStorage.removeItem('user');
-    setUser(null);
-  };
-
-  const getAuthHeader = () => {
-    if (currentUser && currentUser.token) {
-      return { Authorization: `Bearer ${currentUser.token}` };
-    }
-    return {};
-  };
-
-  return (
-    <AuthContext.Provider value={{
-      user,
-      logIn,
-      logOut,
-      getAuthHeader,
-    }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-};
 
 const MainPageRoute = ({ children }) => {
   const auth = useAuth();
--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,11 +1,5 @@
-/* eslint-disable react/jsx-no-constructed-context-values */
-
 import React from 'react';
-import {
-  Provider as StoreProvider,
-  useDispatch,
-  useSelector,
-} from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 import leoProfanity from 'leo-profanity';
@@ -14,84 +8,9 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 
 import store from './slices/index.js';
 import App from './components/App.jsx';
-import { SocketContext } from './contexts/index.js';
-
-import { actions as messagesActions } from './slices/messagesSlice.js';
-import { actions as channelsActions } from './slices/channelsSlice';
+import SocketProvider from './contexts/SocketProvider.jsx';
 
 import resources from './locales/index.js';
-
-const SocketProvider = ({ socket, children }) => {
-  const dispatch = useDispatch();
-
-  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
-
-  const addNewMessage = (message) => socket.emit('newMessage', message, (response) => {
-    if (response.status !== 'ok') {
-      console.log(response.status);
-    }
-  });
-
-  socket.on('newMessage', (payload) => {
-    dispatch(messagesActions.addMessage(payload));
-  });
-
-  const addNewChannel = (channel) => socket.emit('newChannel', channel, (response) => {
-    if (response.status === 'ok') {
-      const { id } = response.data;
-      dispatch(channelsActions.setCurrentChannelId(id));
-    } else {
-      console.log(response.status);
-    }
-  });
-
-  socket.on('newChannel', (payload) => {
-    dispatch(channelsActions.addChannel(payload));
-  });
-
-  const removeChannel = (id) => socket.emit('removeChannel', { id }, (response) => {
-    if (response.status !== 'ok') {
-      console.log(response.status);
-    }
-  });
-
-  socket.on('removeChannel', (payload) => {
-    dispatch(channelsActions.removeChannel(payload));
-    if (payload.id === currentChannelId) {
-      dispatch(channelsActions.setCurrentChannelId(1));
-    } else {
-      dispatch(channelsActions.setCurrentChannelId(currentChannelId));
-    }
-  });
-
-  const renameChannel = (renamedChannel) => socket.emit('renameChannel', renamedChannel, (response) => {
-    if (response.status !== 'ok') {
-      console.log(response.status);
-    }
-  });
-
-  socket.on('renameChannel', (payload) => {
-    const { name, id } = payload;
-    dispatch(channelsActions.changeChannelName({
-      id,
-      changes: {
-        name,
-      },
-    }));
-  });
-
-  return (
-    <SocketContext.Provider value={{
-      addNewMessage,
-      addNewChannel,
-      removeChannel,
-      renameChannel,
-    }}
-    >
-      {children}
-    </SocketContext.Provider>
-  );
-};
 
 const rollbarConfig = {
   accessToken: '9eedeb0568154ea086db04e66f8d26e3',
