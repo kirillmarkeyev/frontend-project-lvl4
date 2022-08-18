1fc37c72839c666182728e79ad0a59a53875847c
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-19 01:43:50 +0300

    Code-review 2: create initSocket.js

---
+++ frontend/src/initSocket.js
@@ -0,0 +1,5 @@
+// import { io } from 'socket.io-client';
+
+// import store from './slices/index.js';
+// import { actions as messagesActions } from './slices/messagesSlice.js';
+// import { actions as channelsActions } from './slices/channelsSlice.js';
