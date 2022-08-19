19dc23cd0ae10a65091443657eead51fe4493c4d
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-19 17:04:33 +0300

    Code-review 2: update initSocket.js

--- frontend/src/initSocket.js
+++ frontend/src/initSocket.js
@@ -1,5 +1,68 @@
-// import { io } from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-// import store from './slices/index.js';
-// import { actions as messagesActions } from './slices/messagesSlice.js';
-// import { actions as channelsActions } from './slices/channelsSlice.js';
+import store from './slices/index.js';
+import { actions as messagesActions } from './slices/messagesSlice.js';
+import { actions as channelsActions } from './slices/channelsSlice.js';
+
+const initSocket = () => {
+  const socket = io();
+
+  const addNewMessage = (message) => socket.emit('newMessage', message, (response) => {
+    if (response.status !== 'ok') {
+      console.log(response.status);
+    }
+  });
+
+  socket.on('newMessage', (payload) => {
+    store.dispatch(messagesActions.addMessage(payload));
+  });
+
+  const addNewChannel = (channel) => socket.emit('newChannel', channel, (response) => {
+    if (response.status === 'ok') {
+      const { id } = response.data;
+      store.dispatch(channelsActions.setCurrentChannelId(id));
+    } else {
+      console.log(response.status);
+    }
+  });
+
+  socket.on('newChannel', (payload) => {
+    store.dispatch(channelsActions.addChannel(payload));
+  });
+
+  const removeChannel = (id) => socket.emit('removeChannel', { id }, (response) => {
+    if (response.status !== 'ok') {
+      console.log(response.status);
+    }
+  });
+
+  socket.on('removeChannel', (payload) => {
+    store.dispatch(channelsActions.removeChannel(payload));
+    store.dispatch(channelsActions.setDefaultChannelId(payload.id));
+  });
+
+  const renameChannel = (renamedChannel) => socket.emit('renameChannel', renamedChannel, (response) => {
+    if (response.status !== 'ok') {
+      console.log(response.status);
+    }
+  });
+
+  socket.on('renameChannel', (payload) => {
+    const { name, id } = payload;
+    store.dispatch(channelsActions.changeChannelName({
+      id,
+      changes: {
+        name,
+      },
+    }));
+  });
+
+  return {
+    addNewMessage,
+    addNewChannel,
+    removeChannel,
+    renameChannel,
+  };
+};
+
+export default initSocket;
