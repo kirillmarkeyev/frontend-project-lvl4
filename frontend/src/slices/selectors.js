78e204a234345ff3dca8f646193de3e3d7ff5b70
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-08-25 00:28:24 +0300

    Code-review 3: add selectors.js

---
+++ frontend/src/slices/selectors.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+
+import { selectors as channelsSelectors } from './channelsSlice.js';
+import { selectors as messagesSelectors } from './messagesSlice.js';
+
+export const getAllChannels = (state) => channelsSelectors.selectAll(state);
+
+export const getCurrentChannelId = (state) => state.channels.currentChannelId;
+
+export const getCurrentChannel = (state) => {
+  const currentChannelId = getCurrentChannelId(state);
+  return channelsSelectors.selectById(state, currentChannelId);
+};
+
+export const getAllMessages = (state) => messagesSelectors.selectAll(state);
+
+export const getModalType = (state) => state.modals.modalType;
+
+export const getItemId = (state) => state.modals.itemId;
